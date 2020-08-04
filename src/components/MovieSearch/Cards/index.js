491bad9fd93b16b9aebad80d2a5b04cc691fbeac
Melissa <melissakheying@gmail.com>
2020-08-04 17:55:34 -0500

    add styles to cards

--- src/components/MovieSearch/Cards/index.js
+++ src/components/MovieSearch/Cards/index.js
@@ -7,7 +7,7 @@ export const Cards = ({ movies }) => {
   const renderMovies = () => {
     return movies.map(movie => {
       return (
-        <div key={movie.id}>
+        <div className='card-container' key={movie.id}>
           <p>{movie.title}</p>
           <img alt='movies' src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
           />
