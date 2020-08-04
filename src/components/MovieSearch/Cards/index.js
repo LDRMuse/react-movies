60459adf459e691ec2a02caba620e4955007877e
Melissa <melissakheying@gmail.com>
2020-08-04 17:34:56 -0500

    display movies after search

--- src/components/MovieSearch/Cards/index.js
+++ src/components/MovieSearch/Cards/index.js
@@ -2,15 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './cards.css'
 
-export const Cards = ({movies}) => {
+export const Cards = ({ movies }) => {
 
-  // movies.map()
+  const renderMovies = () => {
+    return movies.map(movie => {
+      return (
+        <div key={movie.id}>
+          <p>{movie.title}</p>
+        </div>
+      )
+    })
+  }
 
-  // return <div className='card-container'>
-  //   <img alt='movies' src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}/>
-  // </div>
+  return (
+    renderMovies()
+    //   < div className = 'card-container' >
+    //     <img alt='movies' src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+    //     />
+    // </div >
+  )
 
-  return <p>dfg</p>
+
 }
 
 Cards.propTypes = {
--- src/components/MovieSearch/Form/index.js
+++ src/components/MovieSearch/Form/index.js
@@ -1,17 +1,17 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-export const Form = ({handler}) => {
+export const Form = ({ handler }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
     handler(event)
   }
 
-return (
-<form onSubmit={handleSubmit}><input type="search" />
-<button>Submit!</button>
- </form>
+  return (
+    <form onSubmit={handleSubmit}><input type="search" />
+      <button>Submit!</button>
+    </form>
   )
 }
 
