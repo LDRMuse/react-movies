7dc83d179d8897a3423174998fe106e8ef0b5bc1
Melissa <melissakheying@gmail.com>
2020-08-04 16:00:29 -0500

    Match api to query

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ export default {
   async index(query) {
     const res = await fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&page=1&include_adult=false`
-      )
-      return await res.json()
+    )
+    return await res.json()
   },
 }
--- src/components/MovieSearch/Cards/index.js
+++ src/components/MovieSearch/Cards/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './cards.css'
 
 export const Cards = ({movies}) => {
-  return <p>I am a card</p>
+
+  // movies.map()
+
+  // return <div className='card-container'>
+  //   <img alt='movies' src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}/>
+  // </div>
+
+  return <p>dfg</p>
 }
 
 Cards.propTypes = {
