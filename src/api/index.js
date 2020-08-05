5b985e9b64e594704d0500e46e62899cefd5024b
Melissa <melissakheying@gmail.com>
2020-08-05 13:38:53 -0500

    Add throw and catch response to API

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,20 @@ dotenv.config()
 
 export default {
   async index(query) {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&page=1&include_adult=false`
-    )
-    return await res.json()
+    try {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&page=1&include_adult=false`
+      )
+      // fetch will not detect an error, so we create one if code is >400
+      if (res.status > 400) {
+      throw new Error(`This is the throw message. ${res.status}`)
+      }
+
+      return await res.json()
+    } catch (error) {
+      return (`This is an error message from catch. ${error.message}`)
+    }
+
+
   },
 }
