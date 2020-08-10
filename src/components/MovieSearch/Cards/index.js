dd21797eae69262598ded30fc2c15e4705207eb1
Melissa <melissakheying@gmail.com>
2020-08-10 10:44:57 -0500

    Set class to h2

--- src/components/MovieSearch/Cards/index.js
+++ src/components/MovieSearch/Cards/index.js
@@ -2,19 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 
+
+
+
+
+
 export const Cards = ({ movies }) => {
 
   const renderMovies = () => {
     return movies.map(movie => {
       return (
-        <div className='card-container' key={movie.id}>
-          <h2>{movie.title}</h2>
-          <img alt='movies' src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-          />
-          <p>{movie.vote_average} Rating out of 10</p>
-          <p>Release Date: {movie.release_date}</p>
-          <p>Movie Overview: {movie.overview}</p>
-        </div>
+        <div className='has-background-primary box' key={movie.id}>
+<h2 className='is-size-2'>{movie.title}</h2>
+<p>
+<img alt='movies' src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+/></p>
+<p>{movie.vote_average} Rating out of 10</p>
+<p>Release Date: {movie.release_date}</p>
+<p>Movie Overview: {movie.overview}</p>
+</div>
       )
     })
   }
@@ -28,3 +34,4 @@ export const Cards = ({ movies }) => {
 Cards.propTypes = {
   movies: PropTypes.array
 }
+
--- src/components/MovieSearch/Form/index.js
+++ src/components/MovieSearch/Form/index.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+
 export const Form = ({ handler }) => {
 
   const handleSubmit = (event) => {
@@ -9,11 +10,20 @@ export const Form = ({ handler }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-    <h1>Movie Search App</h1>
-    <input type="search" />
-    <button>Search Movies!</button>
+
+    <form className="field" onSubmit={handleSubmit}>
+      <h1 className='title is-1'>Movie Search App</h1>
+
+        <label className="label">Search for your Favorite Movies!</label>
+
+        <div className="control">
+          <input className="input" type="search" placeholder="E.g. Harry Potter" />
+        </div>
+
+      <button className='button is-primary'>Search Movies!</button>
     </form>
+
+
   )
 }
 
