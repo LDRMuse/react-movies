2106dfffcddba1f7e97c4066704a9d4cf3e4bd3b
Melissa <melissakheying@gmail.com>
2020-08-12 16:53:10 -0500

    :lipstick: Add notification class section (header)

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { MovieSearch } from './components'
 
 export const App = () => {
   return (
-    <Fragment>
-      <h1 className='title is-1 has-text-centered'>Movie Search App</h1>
-      <MovieSearch />
+    <Fragment><div className="container">
+    <div className="notification">
+    <h1 className='title is-1 has-text-centered'>Movie Search App</h1>
+    </div>
+  </div>
+
+    <MovieSearch />
     </Fragment>
   );
 }
