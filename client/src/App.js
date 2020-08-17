80d8a8b1f79f908893ba04463516bb7e32b31eed
Melissa <melissakheying@gmail.com>
2020-08-17 15:02:12 -0500

    :construction: restructure for fullstack

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,36 @@
-import React, { useState } from 'react'
+import React, { Fragment } from 'react';
+import './App.css';
+import { MovieSearch } from './components'
 
-import { Cards } from './Cards'
-import { Form as Search } from './Form'
-
-import api from 'api'
-
-export const MovieSearch = () => {
-  const [movies, setMovies] = useState([])
-
-  const searchHandler = async (event) => {
-    const { results } = await api.index(event.target.elements[0].value)
-    setMovies(results)
-  }
-
-
+export const App = () => {
   return (
-    <main className="has-text-centered">
-      <Search handler={searchHandler} />
-      <Cards movies={movies} />
-      <footer className="footer has-text-centered">
-        <div className="columns is-mobile is-centered">
-          <div className="column is-1">
+    <Fragment><div className="container">
+    <div className="notification">
+    <div className="columns is-mobile ">
+          <div className="column is-one-quarter has-background-primary mr-1">
             <a href="https://www.netflix.com/"><img className="image is-32x32" src="https://i.pinimg.com/originals/8c/51/0e/8c510ee7de078ac4eaafdb9d15a810dd.png"
               alt="Netflix" /></a>
           </div>
-          <div className="column is-1">
+          <div className="column is-one-quarter has-background-primary mr-1">
             <a href="https://www.hulu.com/welcome"><img className="image is-32x32" src="https://media.glassdoor.com/sqll/43242/hulu-squarelogo-1537204685767.png"
               alt="Hulu" /></a>
           </div>
-          <div className="column is-1">
+          <div className="column is-one-quarter has-background-primary mr-1">
             <a href="https://www.starz.com/"><img className="image is-32x32" src="https://yt3.ggpht.com/a/AATXAJwlvPYVk2jBSPt9BhzCJLhaviuMfyEXCVAWQ8vf=s900-c-k-c0xffffffff-no-rj-mo"
               alt="starz" /></a>
           </div>
-          <div className="column is-1">
+          <div className="column is-one-quarter has-background-primary mr-1">
             <a href="https://www.disneyplus.com/"><img className="image is-32x32" src="https://lh3.googleusercontent.com/xoGGYH2LgLibLDBoxMg-ZE16b-RNfITw_OgXBWRAPin2FZY4FGB9QKBYApR-0rSCkQ"
               alt="disneyplus" /></a>
           </div>
         </div>
         <div className="content has-text-centered">
         </div>
-      </footer>
-    </main>
-  )
+    <h1 className='title is-1 has-text-centered mt-4 mb-4'>Movie Search App</h1>
+    </div>
+  </div>
+
+    <MovieSearch />
+    </Fragment>
+  );
 }
---
+++ client/src/components/MovieSearch/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+import { Cards } from './Cards'
+import { Form as Search } from './Form'
+
+import api from 'api'
+
+export const MovieSearch = () => {
+  const [movies, setMovies] = useState([])
+
+  const searchHandler = async (event) => {
+    const { results } = await api.index(event.target.elements[0].value)
+    setMovies(results)
+  }
+
+
+  return (
+    <main className="has-text-centered">
+      <Search handler={searchHandler} />
+      <Cards movies={movies} />
+      <img src="https://ae01.alicdn.com/kf/HTB1FVh3XCf2gK0jSZFPq6xsopXaY/Marvel-Comics-Movie-Poster-Retro-Poster-Avengers-Poster-Star-Wars-Poster-Various-Classic-Movie-Posters.jpg_960x960.jpg" />
+      <footer className="footer has-text-centered">
+
+      </footer>
+    </main>
+  )
+}
