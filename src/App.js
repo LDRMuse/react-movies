23a77110daa0616f65530de8c2bafbd200d13081
Melissa <melissakheying@gmail.com>
2020-08-17 17:42:35 -0500

    :lipstick: restyle nav buttons

--- src/App.js
+++ src/App.js
@@ -4,33 +4,30 @@ import { MovieSearch } from './components'
 
 export const App = () => {
   return (
-    <Fragment><div className="container">
-    <div className="notification">
-    <div className="columns is-mobile ">
-          <div className="column is-one-quarter has-background-primary mr-1">
-            <a href="https://www.netflix.com/"><img className="image is-32x32" src="https://i.pinimg.com/originals/8c/51/0e/8c510ee7de078ac4eaafdb9d15a810dd.png"
+    <Fragment>
+      <header className="has-background-light py-4 container">
+        <div className="level">
+          <div className="py-3 level-item has-background-primary">
+            <a href="https://www.netflix.com/"><img className="image is-48x48" src="https://i.pinimg.com/originals/8c/51/0e/8c510ee7de078ac4eaafdb9d15a810dd.png"
               alt="Netflix" /></a>
           </div>
-          <div className="column is-one-quarter has-background-primary mr-1">
-            <a href="https://www.hulu.com/welcome"><img className="image is-32x32" src="https://media.glassdoor.com/sqll/43242/hulu-squarelogo-1537204685767.png"
+          <div className="py-3 level-item has-background-primary">
+            <a href="https://www.hulu.com/welcome"><img className="image is-48x48" src="https://media.glassdoor.com/sqll/43242/hulu-squarelogo-1537204685767.png"
               alt="Hulu" /></a>
           </div>
-          <div className="column is-one-quarter has-background-primary mr-1">
-            <a href="https://www.starz.com/"><img className="image is-32x32" src="https://yt3.ggpht.com/a/AATXAJwlvPYVk2jBSPt9BhzCJLhaviuMfyEXCVAWQ8vf=s900-c-k-c0xffffffff-no-rj-mo"
+          <div className="py-3 level-item has-background-primary">
+            <a href="https://www.starz.com/"><img className="image is-48x48" src="https://yt3.ggpht.com/a/AATXAJwlvPYVk2jBSPt9BhzCJLhaviuMfyEXCVAWQ8vf=s900-c-k-c0xffffffff-no-rj-mo"
               alt="starz" /></a>
           </div>
-          <div className="column is-one-quarter has-background-primary mr-1">
-            <a href="https://www.disneyplus.com/"><img className="image is-32x32" src="https://lh3.googleusercontent.com/xoGGYH2LgLibLDBoxMg-ZE16b-RNfITw_OgXBWRAPin2FZY4FGB9QKBYApR-0rSCkQ"
+          <div className="py-3 level-item has-background-primary">
+            <a href="https://www.disneyplus.com/"><img className="image is-48x48" src="https://lh3.googleusercontent.com/xoGGYH2LgLibLDBoxMg-ZE16b-RNfITw_OgXBWRAPin2FZY4FGB9QKBYApR-0rSCkQ"
               alt="disneyplus" /></a>
           </div>
         </div>
-        <div className="content has-text-centered">
-        </div>
-    <h1 className='title is-1 has-text-centered mt-4 mb-4'>Movie Search App</h1>
-    </div>
-  </div>
+        <h1 className='title is-1 has-text-centered mt-4 mb-4'>Movie Search App</h1>
+      </header>
 
-    <MovieSearch />
+      <MovieSearch />
     </Fragment>
   );
 }
