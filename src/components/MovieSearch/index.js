5752df6e10daaa189c010b0795dae15682df2e85
Melissa <melissakheying@gmail.com>
2020-08-17 17:48:34 -0500

    :bug: Add missing alt tag

--- src/components/MovieSearch/index.js
+++ src/components/MovieSearch/index.js
@@ -10,6 +10,7 @@ export const MovieSearch = () => {
 
   const searchHandler = async (event) => {
     const { results } = await api.index(event.target.elements[0].value)
+    console.log(results);
     setMovies(results)
   }
 
@@ -18,7 +19,7 @@ export const MovieSearch = () => {
     <main className="has-text-centered">
       <Search handler={searchHandler} />
       <Cards movies={movies} />
-      <img src="https://ae01.alicdn.com/kf/HTB1FVh3XCf2gK0jSZFPq6xsopXaY/Marvel-Comics-Movie-Poster-Retro-Poster-Avengers-Poster-Star-Wars-Poster-Various-Classic-Movie-Posters.jpg_960x960.jpg" />
+      <img alt="movie poster" src="https://ae01.alicdn.com/kf/HTB1FVh3XCf2gK0jSZFPq6xsopXaY/Marvel-Comics-Movie-Poster-Retro-Poster-Avengers-Poster-Star-Wars-Poster-Various-Classic-Movie-Posters.jpg_960x960.jpg" />
       <footer className="footer has-text-centered">
 
       </footer>
